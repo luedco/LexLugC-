d898fee4a449269e425a2d94ca29fb7228493adf
luedco <38091075+luedco@users.noreply.github.com>
2021-10-28 23:55:02 -0600

    bases

---
+++ fsharp.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace txtToJson
+{
+    class Program
+    {
+        /*comentario de bloque
+         linea 2 comentario*/
+        static void Main()
+        {
+            int a;
+            a = 15;
+            Console.WriteLine("Hola Mundo\n");
+            Console.WriteLine(a);
+        }
+
+    }
+}
